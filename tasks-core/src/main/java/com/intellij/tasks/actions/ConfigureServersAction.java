d585bf047a6519c2f6f95b3d26f1110d71a75db3
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-02-09 01:55:02 +0300

    reformat

--- tasks-core/src/main/java/com/intellij/tasks/actions/ConfigureServersAction.java
+++ tasks-core/src/main/java/com/intellij/tasks/actions/ConfigureServersAction.java
@@ -1,29 +1,37 @@
 package com.intellij.tasks.actions;
 
+import javax.annotation.Nonnull;
+
 import com.intellij.icons.AllIcons;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.tasks.config.TaskRepositoriesConfigurable;
+import consulo.annotations.RequiredDispatchThread;
 
 /**
  * User: Evgeny Zakrevsky
  */
 
-public class ConfigureServersAction extends BaseTaskAction {
+public class ConfigureServersAction extends BaseTaskAction
+{
+	public ConfigureServersAction()
+	{
+		super("Configure Servers...", null, AllIcons.General.Settings);
+	}
 
-  public ConfigureServersAction() {
-    super("Configure Servers...", null, AllIcons.General.Settings);
-  }
+	@RequiredDispatchThread
+	@Override
+	public void actionPerformed(@Nonnull AnActionEvent e)
+	{
+		TaskRepositoriesConfigurable configurable = new TaskRepositoriesConfigurable(getProject(e));
+		if(ShowSettingsUtil.getInstance().editConfigurable(getProject(e), configurable))
+		{
+			serversChanged();
+		}
+	}
 
-  @Override
-  public void actionPerformed(AnActionEvent e) {
-    TaskRepositoriesConfigurable configurable = new TaskRepositoriesConfigurable(getProject(e));
-    if (ShowSettingsUtil.getInstance().editConfigurable(getProject(e), configurable)) {
-      serversChanged();
-    }
-  }
+	protected void serversChanged()
+	{
 
-  protected void serversChanged() {
-
-  }
+	}
 }
