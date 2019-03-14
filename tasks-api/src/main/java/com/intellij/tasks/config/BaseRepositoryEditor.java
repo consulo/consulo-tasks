cf930c88f198cd24cd769f2fc63b3a2de16c5fe1
Valery Semenchuk <vistall.valeriy@gmail.com>
2019-03-14 10:27:43 +0300

    compilation

--- tasks-api/src/main/java/com/intellij/tasks/config/BaseRepositoryEditor.java
+++ tasks-api/src/main/java/com/intellij/tasks/config/BaseRepositoryEditor.java
@@ -15,10 +15,28 @@
  */
 package com.intellij.tasks.config;
 
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+import javax.annotation.Nullable;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.event.DocumentEvent;
+
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorFactory;
+import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.tasks.TaskManager;
@@ -32,15 +50,7 @@ import com.intellij.ui.components.JBLabel;
 import com.intellij.ui.components.JBTabbedPane;
 import com.intellij.util.Consumer;
 import com.intellij.util.net.HttpConfigurable;
-import javax.annotation.Nullable;
-
-import javax.swing.*;
-import javax.swing.event.DocumentEvent;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import com.intellij.util.net.HttpProxyConfigurable;
 
 /**
  * @author Dmitry Avdeev
@@ -90,8 +100,7 @@ public class BaseRepositoryEditor<T extends BaseRepository> extends TaskReposito
 
     myProxySettingsButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        HttpConfigurable.editConfigurable(myPanel);
-        enableButtons();
+         ShowSettingsUtil.getInstance().editConfigurable(myProject, new HttpProxyConfigurable()).doWhenRejected(() -> enableButtons());
       }
     });
 
